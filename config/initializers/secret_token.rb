eaa70cc93168a0134af0337f0064375e3a803f4c
Isaac Priestley <progressions@gmail.com>
2013-06-28 17:21:52 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adventure::Application.config.secret_key_base = '31c5a6ee90767a54bfd798b882b7a8b483a8d266cdfb9d08c287172271beb8261a17550e8590142dac5b5135866527c2391a8deeae3858d03fd68d36c88d17a9'
