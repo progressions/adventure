d53dec780c8b3326aacd6e1f792501f2d0f8781d
Isaac Priestley <progressions@gmail.com>
2013-06-30 13:35:48 -0500

    add rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Adventure::Application.routes.draw do
   get "game" => 'game#index'
 
+  mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -41,7 +43,7 @@ Adventure::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
